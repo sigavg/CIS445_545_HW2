aa2fe673f316bedf228da077c6d4fe4da1dfedd1
sigavg <lesimplu@gmail.com>
2023-10-21 01:49:15 -0500

    Project 1
    add css, JS, project 1 requirements

---
+++ script.js
@@ -0,0 +1,6 @@
+function turnon(){
+
+    document.getElementById('sw').src = 'onswitch.jpg';
+    document.getElementById('content').removeAttribute('style');
+    document.getElementById('bod').style='background-image: url("background.jpg");'
+}
